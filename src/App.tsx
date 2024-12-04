b91a3ef4d82c5eee05a666696362d65f265d78d7
Wander Pereira da Silva <wanderpereira@gmx.us>
2024-12-04 14:40:47 -0300

    Voltando a cor de Fundo

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { VideoCarousel } from './components/VideoCarousel';
 
 function App() {
   return (
-    <div className="min-h-screen bg-opacity-0">
+    <div className="min-h-screen bg-[#b1d34a99]">
       <div className="container mx-auto px-4 py-8">
         <VideoCarousel />
       </div>
