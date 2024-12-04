621d4c2f75597064c67059eef847b05267c02023
Wander Pereira da Silva <wanderpereira@gmx.us>
2024-12-04 14:43:49 -0300

    Desistindo da cor de Fundo

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { VideoCarousel } from './components/VideoCarousel';
 
 function App() {
   return (
-    <div className="min-h-screen bg-[#b1d34a99]">
-      <div className="container bg-[#b1d34a99] mx-auto px-4 py-8">
+    <div className="min-h-screen">
+      <div className="container mx-auto px-4 py-8">
         <VideoCarousel />
       </div>
     </div>
