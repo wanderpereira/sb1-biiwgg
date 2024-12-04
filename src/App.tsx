6d1606470fde8e5f40201d8ce38602e593aed45c
Wander Pereira da Silva <wanderpereira@gmx.us>
2024-12-04 14:42:12 -0300

    Adicionando cor de fundo ao elemento secundário

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { VideoCarousel } from './components/VideoCarousel';
 function App() {
   return (
     <div className="min-h-screen bg-[#b1d34a99]">
-      <div className="container mx-auto px-4 py-8">
+      <div className="container bg-[#b1d34a99] mx-auto px-4 py-8">
         <VideoCarousel />
       </div>
     </div>
