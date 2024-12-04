3bd55a0ce073de3187c29141191d9138a22cb6b7
Wander Pereira da Silva <wanderpereira@gmx.us>
2024-12-04 12:27:27 -0300

    Apagando Título
    Retirado o título do cabeçalho

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,8 @@ import { VideoCarousel } from './components/VideoCarousel';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100">
+    <div className="min-h-screen bg-white-100">
       <div className="container mx-auto px-4 py-8">
-        <h1 className="mb-8 text-center text-3xl font-bold">Confira nossos últimos vídeos</h1>
         <VideoCarousel />
       </div>
     </div>
